0b272b143a8ed030fd816e0f06e24f510c815df4
Luc Engelen <luc@kabisa.nl>
2019-01-11 16:38:40 +0100

    Add script to run latest Jenkins in Docker container

---
+++ run-jenkins-with-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker pull jenkinsci/blueocean
+docker run -u root --rm -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins-data:/var/jenkins_home \
+  -v jenkins-root:/root \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /Users/lucengelen/Repositories:/var/repositories \
+  jenkinsci/blueocean
