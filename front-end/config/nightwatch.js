d765b1668909fe11db4e10190249bc08237e4850
Luc Engelen <luc@kabisa.nl>
2019-08-05 12:10:33 +0200

    Use Chromedriver directly

--- front-end/config/nightwatch.js
+++ front-end/config/nightwatch.js
@@ -1,22 +1,17 @@
-const selenium = require("selenium-server-standalone-jar");
 const chromedriver = require("chromedriver");
 
 module.exports = {
   src_folders: ["./test/e2e"],
   output_folder: "reports",
-  selenium: {
+  webdriver: {
     start_process: true,
-    server_path: selenium.path,
+    server_path: chromedriver.path,
     log_path: "reports",
-    cli_args: {
-      "webdriver.chrome.driver": chromedriver.path
-    }
+    port: 9515
   },
   test_settings: {
     default: {
       launch_url: `http://localhost:${process.env.SERVER_PORT || 9091}/`,
-      selenium_port: 4444,
-      selenium_host: "localhost",
       desiredCapabilities: {
         browserName: "chrome",
         chromeOptions: {
--- front-end/script/test-e2e.js
+++ front-end/script/test-e2e.js
@@ -2,20 +2,6 @@
 
 const child_process = require("child_process");
 
-const hasJava = function() {
-  return child_process.spawnSync("java", ["-version"]).status == 0;
-};
-
-// workaround for issue https://github.com/nightwatchjs/nightwatch/issues/1454
-// prevents false positives.
-// Can be skipped if selenium server is disabled in nightwatch
-if (!hasJava()) {
-  const colorRed = "\x1b[31m";
-  const colorReset = "\x1b[0m";
-  console.error(`${colorRed}JAVA not found. Exiting${colorReset}`);
-  process.exit(1);
-}
-
 const server = require("./dev-server.js");
 
 const testOptions = process.argv.slice(2);
